df5f3f5a61b4a186570346ea2ab5f70fa2cbad1b
Darwin Robinson <Darwin.Robinson8@gmail.com>
2024-06-20 14:05:57 -0400

    cors config prod

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,11 @@
 # config/initializers/cors.rb
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'localhost:3001'# Replace with your actual frontend domain
-      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    end
+  allow do
+    origins 'localhost:3001', 'https://nomadroof-front-end-app.onrender.com/'
+    # Replace with your actual frontend domains separated by comma if needed
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
+end
+
   
